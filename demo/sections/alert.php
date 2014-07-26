9fc493fc01cfd43a440c50126a1539f102150526
Olivier <olivier.sazos@gmail.com>
2014-07-26 11:15:02 +0200

    Move alert() $dismissible param into the options array, and rename it "dismiss"

--- demo/sections/alert.php
+++ demo/sections/alert.php
@@ -3,20 +3,20 @@
 
 <div class="panel panel-default">
 	<div class="panel-heading">
-		Usage: <code>alert($content, $options, $dismisible = false)</code>
+		Usage: <code>alert($content, $options)</code>
 	</div>
 	<div class="panel-body">
 		<pre class="syntax html">&lt;?php
 $content = "&lt;strong&gt;Well done!&lt;/strong&gt; You successfully read this important alert message.";
 echo $Tbs-&gt;alert($content, array('type' =&gt; 'success'));
-echo $Tbs-&gt;alert($content, array('type' =&gt; 'success'), 'Close');
+echo $Tbs-&gt;alert($content, array('type' =&gt; 'success', 'dismiss' =&gt; 'Close'));
 ?&gt;</pre>
 	</div>
 	<div class="panel-footer">
 		<?php
 		$content = "<strong>Well done!</strong> You successfully read this important alert message.";
 		echo $Tbs->alert($content, array('type' => 'success'));
-		echo $Tbs->alert($content, array('type' => 'warning'), 'Close');
+		echo $Tbs->alert($content, array('type' => 'warning', 'dismiss' => 'Close'));
 		?>
 	</div>
 </div>
